5ddf16c3fde74166dc3a28f222292f8deefa79fa
aokon <andrzej.okon@ceri.pl>
2017-04-25 13:46:15 +0200

    refactor3

---
+++ src/main/resources/database/H2-dataload.sql
@@ -0,0 +1,4 @@
+INSERT INTO USER(name, country) VALUES('Michael Jordan1', 'USA');
+INSERT INTO USER(name, country) VALUES('Michael Jordan2', 'USA');
+INSERT INTO USER(name, country) VALUES('Michael Jordan3', 'USA');
+commit;
